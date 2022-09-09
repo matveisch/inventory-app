2368a6f9828406e953305139623ed66e389ee1e5
Matvei <matvesch@protonmail.com>
2022-09-09 19:37:08 +0300

    delete category

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -41,20 +41,11 @@ exports.category_details = (req, res, next) => {
     )
 };
 
-exports.item_create_get = (req, res, next) => {
-    Category.find({}).exec((err, categories) => {
-        if (err) return next(err);
-
-        console.log(categories);
-
-        res.render('item_form', {
-            title: 'Create Item',
-            categories,
-        });
-    });
+exports.category_create_get = (req, res) => {
+    res.render('category_form', {title: 'Create Category'});
 };
 
-exports.item_create_post = [
+exports.category_create_post = [
     body("name", "Name must not be empty.")
         .trim()
         .isLength({ min: 1 })
@@ -63,69 +54,88 @@ exports.item_create_post = [
         .trim()
         .isLength({ min: 1 })
         .escape(),
-    body("category.*", "Category must not be empty.")
-        .escape(),
-    body("price", "Price must not be empty")
-        .trim()
-        .isInt({ min: 0})
-        .escape(),
-    body("numberInStock", "Number In Stock must not be empty")
-        .trim()
-        .isInt({ min: 0})
-        .escape(),
     (req, res, next) => {
         const errors = validationResult(req);
 
-        const item = new Item({
+        const category = new Category({
             name: req.body.name,
             description: req.body.description,
-            category: req.body.category,
-            price: req.body.price,
-            numberInStock: req.body.numberInStock,
         });
 
         if (!errors.isEmpty()) {
-            Category.find({}).exec((err, categories) => {
-                if (err) return next(err);
-
-                res.render('item_form', {
-                    title: 'Create Item',
-                    categories,
-                    item,
-                    errors: errors.array(),
-                });
-            })
+            res.render('category_form', {
+                title: 'Create Category',
+                category,
+                errors: errors.array(),
+            });
 
             return;
         }
 
-        item.save((err) => {
+        category.save((err) => {
             if (err) return next(err);
 
-            res.redirect(item.url);
+            res.redirect(category.url);
         });
     }
 ];
 
-exports.item_delete_get = (req, res, next) => {
-    Item.findById(req.params.id).exec((err, item) => {
-        if (err) return next(err);
+exports.category_delete_get = (req, res, next) => {
+    async.parallel(
+        {
+            category(callback) {
+                Category.findById(req.params.id).exec(callback);
+            },
+            category_items(callback) {
+                Item.find({category: req.params.id}).exec(callback);
+            }
+        },
+        (err, results) => {
+            if (err) return next(err);
 
-        if (item === null) res.redirect('catalog/items');
+            if (results.category === null) res.redirect('catalog/categories');
 
-        res.render('item_delete', {
-            title: 'Delete Item',
-            item
-        })
-    })
+            res.render('category_delete', {
+                title: 'Delete Category',
+                category: results.category,
+                category_items: results.category_items,
+            })
+        }
+    )
 };
 
-exports.item_delete_post = (req, res, next) => {
-    Item.findByIdAndRemove(req.body.itemid, (err) => {
-        if (err) return next(err);
+exports.category_delete_post = (req, res, next) => {
+    async.parallel(
+        {
+            category(callback) {
+                Category.findById(req.params.id).exec(callback);
+            },
+            category_items(callback) {
+                Item.find({category: req.params.categoryid}).exec(callback);
+            }
+        },
+        (err, results) => {
+            if (err) return next(err);
 
-        res.redirect('/catalog/items');
-    })
+            console.log(results.category_items);
+
+            if (results.category_items.length > 0) {
+                res.render('category_delete', {
+                    title: 'Delete Category',
+                    category: results.category,
+                    category_items: results.category_items,
+                });
+
+                return;
+            }
+
+            Category.findByIdAndRemove(req.body.categoryid, (err) => {
+                if (err) return next(err);
+
+                res.redirect('/catalog/categories');
+            })
+        }
+    )
 }
 
 exports.item_update_get = (req, res, next) => {
--- routes/catalog.js
+++ routes/catalog.js
@@ -18,11 +18,11 @@ router.post('/item/:id/update', item_controller.item_update_post);
 
 // category routes
 router.get('/categories', category_controller.categories_list);
-// router.get('/category/create', category_controller.category_create_get);
-// router.post('/category/create', category_controller.category_create_post);
+router.get('/category/create', category_controller.category_create_get);
+router.post('/category/create', category_controller.category_create_post);
 router.get('/category/:id', category_controller.category_details);
-// router.get('/category/:id/delete', category_controller.category_delete_get);
-// router.post('/category/:id/delete', category_controller.category_delete_post);
+router.get('/category/:id/delete', category_controller.category_delete_get);
+router.post('/category/:id/delete', category_controller.category_delete_post);
 // router.get('/category/:id/update', category_controller.category_update_get);
 // router.post('/category/:id/update', category_controller.category_update_post);
 
